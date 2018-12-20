7b98ebd817c249c087dec40d758e47d2685dda0a
dvckbass <duckbass@live.com>
2018-12-20 13:00:55 +0100

    bessere Struktur

--- src/webcrawler/WebCrawler.java
+++ src/webcrawler/WebCrawler.java
@@ -1,150 +1,175 @@
 package webcrawler;
 
-
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.net.MalformedURLException;
-import java.nio.file.FileSystems;
-import java.nio.file.Files;
-import java.nio.file.Path;
+import java.sql.Savepoint;
 import java.util.Properties;
 
 import org.apache.commons.io.FileUtils;
 
-import com.gargoylesoftware.htmlunit.FailingHttpStatusCodeException;
 import com.gargoylesoftware.htmlunit.ProxyConfig;
 import com.gargoylesoftware.htmlunit.WebClient;
 import com.gargoylesoftware.htmlunit.html.HtmlPage;
 
+/**
+ * Dient als Klasse für Web-Crawling, die eine Konfiguration benötigen
+ * 
+ * @author Ardilla Latifasari
+ *
+ */
+
 public class WebCrawler {
-	
-	private String proxyHost = "";
-	private int proxyPort = 0;
-	private String fileString = "";
-	
- char[] replaceChar = {'<','>',':','"','/', '\\', '|', '?', '*'};
 
-    public static void main(String[] args) throws FailingHttpStatusCodeException, MalformedURLException, IOException {
-    	WebCrawler webCrawler = new WebCrawler();
-    	
-    	webCrawler.loadProp();
-    	
-    	try (final WebClient webClient = new WebClient()) {
-    		String webString;
-    		ProxyConfig proxyConfig = new ProxyConfig(webCrawler.getProxyHost(), webCrawler.getProxyPort());
-    		webClient.getOptions().setProxyConfig(proxyConfig);
-    		webClient.getOptions().setJavaScriptEnabled(false);
-    		webClient.getOptions().setThrowExceptionOnFailingStatusCode(false);
-    		webClient.getOptions().setThrowExceptionOnScriptError(false);
-    		webClient.getOptions().setUseInsecureSSL(true);
-    		
-    		for(int c=0;c<args.length;c++) {
-    			webString = args[c];
-    			System.out.println("webstring: " + webString);
-    			webCrawler.setFileString(webCrawler.removeChar(webString));
-                final HtmlPage page = webClient.getPage(webString);
-                
-                /*
-                Path path = Paths.get("Data\\" + webCrawler.getFileString());
-                try {
-        			System.out.println("! Deleting File From The Path !");
-        			if(!Files.notExists(path)) {
-        				Files.delete(path);
-        			}
-                	page.save(path.toFile());
-                	System.out.println(webCrawler.getFileString() + " saved.");
-        		} catch(IOException ioException) {
-        			ioException.printStackTrace();
-        		}
-        		*/
-                
-                File fileDir = new File("Data\\" + webCrawler.getFileString());
-                File file = new File("Data\\" + webCrawler.getFileString() + ".html");
-                System.out.println("File String: " + webCrawler.getFileString());
+	private String proxyHost;
+	private int proxyPort;
+	private String fileString;
+	private String webString;
 
-            	if(file.exists()) {
-            		file.delete();
-            		System.out.println(file.getName() + "deleted.");
-            		FileUtils.deleteDirectory(fileDir);
-            		System.out.println(fileDir.getName() + "deleted.");
-            	}     
-            	page.save(file);
-            	System.out.println(webCrawler.getFileString() + " saved.");
-            	
-    		}
-    		
-    		System.out.println("SUCCESS");
-    		
-        }
-        
-    }
-    
-    public String getProxyHost() {
+	public static final String DEFAULT_DOWNLOAD_DIR = "./sites/";
+	public static final String PROPERTY_FILE = "config.properties";
+	public static final String PROPERTY_PROXYHOST = "proxyhost";
+	public static final String PROPERTY_PROXYPORT = "proxyport";
+
+	private File fileDir;
+	private File file;
+
+	private static final WebClient webClient = new WebClient();
+	// ungültige Sonderzeichen bei der URL
+	private char[] replaceChar = { '<', '>', ':', '"', '/', '\\', '|', '?', '*' };
+
+	public static void main(String[] args) throws IOException {
+		WebCrawler webCrawler = new WebCrawler();
+		webCrawler.loadProp();
+		System.out.println("Property loaded");
+		webCrawler.config();
+		System.out.println("Web client configured");
+		webCrawler.savePage(args);
+	}
+
+	public void setProxyHost(String proxyHost) {
+		this.proxyHost = proxyHost;
+	}
+
+	public String getProxyHost() {
 		return proxyHost;
 	}
-    
-    public int getProxyPort() {
+
+	public void setProxyPort(int proxyPort) {
+		this.proxyPort = proxyPort;
+	}
+
+	public int getProxyPort() {
 		return proxyPort;
 	}
-    
-    public void setFileString(String fileString) {
+
+	public void setFileString(String fileString) {
 		this.fileString = fileString;
 	}
-    
-    public String getFileString() {
+
+	public String getFileString() {
 		return fileString;
 	}
- 
-    private void loadProp() {
-    	Properties prop = new Properties();
-    	InputStream input = null;
 
-    	try {
+	/**
+	 * Zugreifen auf die Konfiguration-Datei
+	 * 
+	 * @throws IOException
+	 */
+	public void loadProp() throws IOException {
+		Properties prop = new Properties();
+		InputStream input = null;
 
-    		input = new FileInputStream("config.properties");
+		input = new FileInputStream(PROPERTY_FILE);
+		prop.load(input);
 
-    		// load a properties file
-    		prop.load(input);
+		proxyHost = prop.getProperty(PROPERTY_PROXYHOST);
+		proxyPort = Integer.parseInt(prop.getProperty(PROPERTY_PROXYPORT));
+	}
 
-    		// get the property value and print it out
-    		proxyHost = prop.getProperty("proxyhost");
-    		proxyPort = Integer.parseInt(prop.getProperty("proxyport"));
+	/**
+	 * Entfernt die ungültige Sonderzeichen zum Speichern der Datei
+	 * 
+	 * @param src
+	 * URL der Seite als String
+	 * 
+	 * @return
+	 * gültige Sonderzeichen als String
+	 */
+	public String removeChar(String src) {
+		char[] srcArr = src.toCharArray();
+		StringBuilder sb = new StringBuilder();
+		for (int i = 0; i < srcArr.length; i++) {
+			char foundChar = isFound(srcArr[i]);
+			if (foundChar != '\0')
+				sb.append(foundChar);
+		}
+		return sb.toString();
+	}
 
-    	} catch (IOException ex) {
-    		ex.printStackTrace();
-    	} finally {
-    		if (input != null) {
-    			try {
-    				input.close();
-    			} catch (IOException e) {
-    				e.printStackTrace();
-    			}
-    		}
-    	}
+	/**
+	 * Suchen nach einem char
+	 * 
+	 * @param src
+	 * @return
+	 */
+	public char isFound(char src) {
+		for (int i = 0; i < replaceChar.length; i++) {
+			if (src == replaceChar[i]) {
+				return '\0';
+			}
+		}
+		return src;
+	}
+	
+	/**
+	 * Konfigurieren des WebClients
+	 */
 
-    }
+	public void config() {
+		ProxyConfig proxyConfig = new ProxyConfig(getProxyHost(), getProxyPort());
+		webClient.getOptions().setProxyConfig(proxyConfig);
+		webClient.getOptions().setJavaScriptEnabled(false);
+		webClient.getOptions().setThrowExceptionOnFailingStatusCode(false);
+		webClient.getOptions().setThrowExceptionOnScriptError(false);
+		webClient.getOptions().setUseInsecureSSL(true);
+	}
+	
+	/**
+	 * Speichern der Seite
+	 * 
+	 * @param args
+	 * URL Seiten, die heruntergeladen würden
+	 * 
+	 * @throws IOException
+	 */
 
-    private String removeChar(String src){
-        char[] srcArr = src.toCharArray(); 
-        StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < srcArr.length; i++) {
-            char foundChar = isFound(srcArr[i]);
-            if(foundChar!='\0')
-            sb.append(foundChar);           
-        }
-        return sb.toString();
+	public void savePage(String[] args) throws IOException {
+		for (int c = 0; c < args.length; c++) {
+			webString = args[c];
+			System.out.println("webstring: " + webString);
+			setFileString(removeChar(webString));
+			final HtmlPage page = webClient.getPage(webString);
 
-    } 
+			fileDir = new File(DEFAULT_DOWNLOAD_DIR + getFileString());
+			if (getFileString().matches("^.*html$")) {
+				file = new File(DEFAULT_DOWNLOAD_DIR + getFileString());
+			} else {
+				file = new File(DEFAULT_DOWNLOAD_DIR + getFileString() + ".html");
+			}
 
-    private char isFound(char src){      
-        for (int i = 0; i < replaceChar.length; i++) {
-            if(src==replaceChar[i]){
-                return '\0';
-            }
-        }
-        return src;
-    }
+			if (file.exists()) {
+				file.delete();
+				System.out.println(file.getName() + "deleted.");
+				FileUtils.deleteDirectory(fileDir);
+				System.out.println(fileDir.getName() + "deleted.");
+			}
+			page.save(file);
+			System.out.println(getFileString() + " saved.");
+
+		}
+
+	}
 
 }
